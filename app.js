abb067991ab69b496ab9eea1af7537b397e3c081
Dustin Kimmel <dkimmel@gmail.com>
2016-08-12 14:58:42 -0700

    Begin Exercise

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // TODO
+// var App = () => (
+//   <div>Some cliche salutation</div>
+// );
+
+// var Cucumbers = () => (
+//   <li>
+//     Cucumbers
+//   </li>
+// );
+
+// var GroceryListItem = () => (
+//   <li>
+    
+//   </li>
+// );
+
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+      <GroceryListItem items={["kale", "shrimp", "peas"]}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
